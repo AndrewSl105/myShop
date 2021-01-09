4e1321b1643cbcdbbc09de816ccf506017022746
Andrii Slipchenko <a.slipchenko@aimprosoft.com>
2021-01-09 20:14:49 +0200

    Updates

---
+++ precache-manifest.7595d159268fadbbed47408522b4c019.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "15a510f412e1dbe8b1f6",
+    "url": "/myShop/static/css/main.1b8e92f0.chunk.css"
+  },
+  {
+    "revision": "15a510f412e1dbe8b1f6",
+    "url": "/myShop/static/js/main.15a510f4.chunk.js"
+  },
+  {
+    "revision": "abbdb520985ab79b78cc",
+    "url": "/myShop/static/css/1.01858104.chunk.css"
+  },
+  {
+    "revision": "abbdb520985ab79b78cc",
+    "url": "/myShop/static/js/1.abbdb520.chunk.js"
+  },
+  {
+    "revision": "38b6a187486178750e25",
+    "url": "/myShop/static/js/runtime~main.38b6a187.js"
+  },
+  {
+    "revision": "af0f64700b14df636a36470e898c660d",
+    "url": "/myShop/static/media/logo.af0f6470.png"
+  },
+  {
+    "revision": "62265274e3316fa5f935237ea92394c1",
+    "url": "/myShop/static/media/cart.62265274.svg"
+  },
+  {
+    "revision": "c7a33805ffda0d32bd2a9904c8b02750",
+    "url": "/myShop/static/media/color.c7a33805.png"
+  },
+  {
+    "revision": "3b6e3706f42d8876fe364ab4d75683fd",
+    "url": "/myShop/static/media/primeicons.3b6e3706.woff"
+  },
+  {
+    "revision": "121254f73060bcbb53ca13258dbd134f",
+    "url": "/myShop/static/media/primeicons.121254f7.ttf"
+  },
+  {
+    "revision": "b0f5d02f4e70967dcc8162e938484366",
+    "url": "/myShop/static/media/primeicons.b0f5d02f.eot"
+  },
+  {
+    "revision": "25954ab5e32c01d577dcafd035597f5d",
+    "url": "/myShop/static/media/primeicons.25954ab5.svg"
+  },
+  {
+    "revision": "0b70e162a1194eb1816c6739d55211c2",
+    "url": "/myShop/index.html"
+  }
+];
