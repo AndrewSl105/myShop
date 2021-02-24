14616aa577dddfc3ce66e5479d6da72190b11e76
Andrey Slipchenko <a.slipchenko@aimprosoft.com>
2021-02-24 21:07:03 +0200

    drop

--- client/src/components/admin-components/admin-pages/admin-theaming/adminTheeming.js
+++ client/src/components/admin-components/admin-pages/admin-theaming/adminTheeming.js
@@ -1,18 +1,11 @@
-import React, { useState }  from 'react';
+import React, { useState, useEffect }  from 'react';
 import { RadioButton } from 'primereact/radiobutton';
 import './adminTheeming.sass';
 import { connect } from 'react-redux';
 import {upgrateNavStyles} from '../../../../redux/actions'
-
-
+import axios from "axios";
+import DataFrom from './datafrom'
 const AdminTheeming = (props) => {
-    const [navStyle, setNavStyle] = useState(null);
-
-    
-
-    console.log(props.styles)
-
-   
 
     const changeStyles = () => {
         
@@ -21,29 +14,7 @@ const AdminTheeming = (props) => {
     return (
         <div className='theeme-container'>
             <div className='theeme'>
-                <div className="p-field-radiobutton">
-                    <RadioButton 
-                        inputId="horizont"
-                        name="nav-style"
-                        value="Horizont"
-                        onChange={(e) => setNavStyle(e.value)} 
-                        checked={navStyle === 'Horizont'} 
-                       />
-                    <label htmlFor="city1"> Horizont</label>
-                </div>
-                <div className="p-field-radiobutton">
-                    <RadioButton 
-                        inputId="vertical" 
-                        name="nav-style"
-                        value="Vertical" 
-                        onChange={(e) => setNavStyle(e.value)} 
-                        checked={navStyle === 'Vertical'} 
-                    />
-                    <label htmlFor="city2">Vertical</label>
-                </div>
-                <button onClick={changeStyles} >
-                    Add styles
-                </button>
+                <DataFrom />
             </div>
         </div>
     )
@@ -52,7 +23,6 @@ const AdminTheeming = (props) => {
 const mapStateToPropsTotal = (state) => {
     let styleResult = Object.values(state.styles).flat(1)
 
-    console.log(styleResult)
 
     return {
         styles: styleResult
