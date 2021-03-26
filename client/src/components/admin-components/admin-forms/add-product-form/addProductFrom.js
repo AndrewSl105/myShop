26056d5690b67504dc99df854fbd209357b3c3ec
Andrey Slipchenko <slip141@gmail.com>
2021-03-26 13:26:58 +0200

    dsfsf

--- client/src/components/admin-components/admin-forms/add-product-form/addProductFrom.js
+++
@@ -1,7 +0,0 @@
-import { compose } from 'recompose';
-import { graphql } from 'react-apollo';
-
-import { addProductMutation  } from './mutations';
-
-
-export default compose(graphql(addProductMutation ));
--- client/src/components/product-list/product-list/productListHoc.js
+++
@@ -1,7 +0,0 @@
-import { compose } from 'recompose';
-import { graphql } from 'react-apollo';
-
-import { productsQuery } from './queries';
-
-
-export default compose(graphql(productsQuery));
